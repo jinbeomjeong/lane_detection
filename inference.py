495079b26fa112aabe72b3529e38c2946235765b
jinbeom, jeong <ppgt1225@naver.com>
2023-12-04 21:31:04 +0900

    fix calulcation of fps

--- inference.py
+++ inference.py
@@ -8,7 +8,8 @@ from model import SCNN
 from utils.transforms import Resize, Compose, ToTensor, Normalize
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # 모델이 gpu 에서 동작하도록 (gpu가 없으면 cpu)
-net = SCNN(input_size=(512, 288), pretrained=False)
+input_size = (512, 288)
+net = SCNN(input_size=input_size, pretrained=False)
 net.to(device) # 모델이 gpu 에서 동작하도록 (gpu가 없으면 cpu)
 
 mean = (0.3598, 0.3653, 0.3662) # CULane 데이터셋의 mean, std
@@ -17,10 +18,12 @@ std = (0.2573, 0.2663, 0.2756)
 # mean = (0.485, 0.456, 0.406) # Imagenet 데이터셋의 mean, std
 # std = (0.229, 0.224, 0.225)
 
-transform_img = Resize((512, 288))
+transform_img = Resize(input_size)
 transform_to_net = Compose(ToTensor(), Normalize(mean=mean, std=std))
 
 color = np.array([[255, 255, 255], [150, 150, 150], [0, 0, 255], [30, 30, 255]], dtype='uint8')
+
+mask_img = cv2.imread(filename="mask_image.jpg")
 
 
 def parse_args():
@@ -45,31 +48,29 @@ def main():
     net.load_state_dict(save_dict['net'])
     net.eval()
 
-    cap = cv2.VideoCapture('d:\\video\\urban_street.mp4')
+    cap = cv2.VideoCapture('d:\\video\\img_1580.MOV')
 
     while cap.isOpened():
-        img_process_start = time.time()
+        t0 = time.time()
         ret, frame = cap.read()
+        # frame = cv2.resize(src=frame, dsize=(960, 540), interpolation=cv2.INTER_AREA)
+        print(frame.shape)
 
         if ret is False or frame is None:
             break
-        print(frame.shape)
+
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         img = transform_img({'img': img})['img']
         x = transform_to_net({'img': img})['img']
         x.unsqueeze_(0)
-        img_process_fps = 1 / (time.time() - img_process_start)
-        # print("fps-image_processing: ", img_process_fps)
 
-        inference_start = time.time()
         seg_pred, exist_pred = net(x.to(device))[:2]
         seg_pred = seg_pred.detach().cpu().numpy()
-        exist_pred = exist_pred.detach().cpu().numpy()
         seg_pred = seg_pred[0]
-        exist = [1 if exist_pred[0, i] > exist_threshold else 0 for i in range(4)]
 
-        inference_fps = 1 / (time.time() - inference_start)
-        print("inference fps: ", inference_fps)
+        exist_pred = exist_pred.detach().cpu().numpy()
+
+        exist = [1 if exist_pred[0, i] > exist_threshold else 0 for i in range(4)]
 
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         lane_img = np.zeros_like(img)
@@ -78,17 +79,26 @@ def main():
 
         for i in range(0, 4):
             if exist_pred[0, i] > exist_threshold:
-                lane_img[coord_mask == (i + 1)] = color[i]
+                lane_img[coord_mask == (i + 1)] = color[0]
 
-        lane_img = cv2.resize(lane_img, (frame.shape[1], frame.shape[0]), interpolation=cv2.INTER_NEAREST)
+        fps = 1 / (time.time() - t0)
+        print("fps-image_processing: ", fps)
+
+        lane_img = cv2.resize(lane_img, (frame.shape[1], frame.shape[0]), interpolation=cv2.INTER_LINEAR)
         result_frame = cv2.addWeighted(src1=lane_img, alpha=0.8, src2=frame, beta=1., gamma=0.)
+        #print(result_frame.shape)
+        # result_frame = cv2.addWeighted(src1=mask_img, alpha=0.8, src2=result_frame, beta=1.0, gamma=0.0)
 
-        # for x in getLane.prob2lines_CULane(seg_pred, exist):
+        # for x in getLane.prob2lines_CULane(seg_pred, exist):q
         #     print(x)
 
+
+
         if args.visualize:
-            # print([1 if exist_pred[0, i] > exist_threshold else 0 for i in range(4)])
+            #print([1 if exist_pred[0, i] > exist_threshold else 0 for i in range(4)])
             cv2.imshow("frame", result_frame)
+            #cv2.imshow('1', roi(result_frame))
+
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
